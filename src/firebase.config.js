ddb6e17cc99fb25f593644504267c137c3c77da3
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-05-21 19:13:26 -0400

    iniciando workflow

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,11 +6,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBAYk_WMmRpvugRq7BSv9aChHKk7B_YjP4',
-  authDomain: 'proyecto-prueba-ec5fc.firebaseapp.com',
-  projectId: 'proyecto-prueba-ec5fc',
-  storageBucket: 'proyecto-prueba-ec5fc.appspot.com',
-  messagingSenderId: '907102392422',
-  appId: '1:907102392422:web:f59b709c5c8e8ec66b9da7',
-  measurementId: 'G-7FQZWB4Q94',
+  apiKey: "AIzaSyBvQXedrQtBMHcFYFnuIQkTZGzO40pPfXA",
+  authDomain: "be-my-friend-f9f09.firebaseapp.com",
+  projectId: "be-my-friend-f9f09",
+  storageBucket: "be-my-friend-f9f09.appspot.com",
+  messagingSenderId: "95083687616",
+  appId: "1:95083687616:web:a41b0451f2c0cc8dfac725"
 };
