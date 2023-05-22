457fdd8221a55b7d700d9291caebfc1faa78967b
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-05-21 20:46:32 -0400

    corrección

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,10 +6,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBvQXedrQtBMHcFYFnuIQkTZGzO40pPfXA",
-  authDomain: "be-my-friend-f9f09.firebaseapp.com",
-  projectId: "be-my-friend-f9f09",
-  storageBucket: "be-my-friend-f9f09.appspot.com",
-  messagingSenderId: "95083687616",
-  appId: "1:95083687616:web:a41b0451f2c0cc8dfac725"
+  apiKey: "AIzaSyBQHbm81siG4gBlefMgD7ufDM62bawS0pg",
+  authDomain: "be-my-friend-820a5.firebaseapp.com",
+  projectId: "be-my-friend-820a5",
+  storageBucket: "be-my-friend-820a5.appspot.com",
+  messagingSenderId: "73696263527",
+  appId: "1:73696263527:web:1b6b44a143657e97fa83c2"
 };
